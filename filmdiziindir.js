0e09ca31c0d69892c2d391416420e54b662b68fb
canyouseem3 <46871655+canyouseem3@users.noreply.github.com>
2020-10-13 23:13:23 +0300

    Update filmdiziindir.js

--- filmdiziindir.js
+++ filmdiziindir.js
@@ -22,9 +22,9 @@ var end   = false;
 				end = split[0]+" "+split[1]+" "+split[2]+" "+split[3]+" "+split[4]+" "+split[5]+" "+split[6]+" "+split[7]+" "+split[8];
  			}else if(split.length >= 10){
 				end = split[0]+" "+split[1]+" "+split[2]+" "+split[3]+" "+split[4]+" "+split[5]+" "+split[6]+" "+split[7]+" "+split[8]" "+split[9];
-      }else if(split.length >= 11){
+     		 	}else if(split.length >= 11){
 				end = split[0]+" "+split[1]+" "+split[2]+" "+split[3]+" "+split[4]+" "+split[5]+" "+split[6]+" "+split[7]+" "+split[8]" "+split[9]" "+split[10];
-      }else if(split.length >= 12){
+     			}else if(split.length >= 12){
 				end = split[0]+" "+split[1]+" "+split[2]+" "+split[3]+" "+split[4]+" "+split[5]+" "+split[6]+" "+split[7]+" "+split[8]" "+split[9]" "+split[10]" "+split[11];
 			}else{
 				end = document.title;
