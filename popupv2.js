eedccb4360c9c5972f6c34348405f4f8c43e0e61
canyouseem3 <46871655+canyouseem3@users.noreply.github.com>
2022-03-25 16:07:47 +0300

    Update popupv2.js

--- popupv2.js
+++ popupv2.js
@@ -1,59 +1,84 @@
-<script type="text/javascript">
-var puShown = false;
-        function doOpen(url)
-        {
-                if ( puShown == true )
-                {
-                        return true;
-                }
-             win = window.open(url,"yenipencere","menubar=0,resizable=1,width=1,height=1");
-             win.moveTo(150000, 150000); 
-                if ( win )
-                {
-                        win.blur();
-                        puShown = true;
-                }
-                return win;
-        }
-        function setCookie(name, value, time)
-        {
-            var expires = new Date();
-            expires.setTime( expires.getTime() + time );
-            document.cookie = name + '=' + value + '; expires=' + expires.toGMTString();
-        }
-       function getCookie(name) { 
-var cookies = document.cookie.toString().split('; '); 
-var cookie, c_name, c_value; 
-// eğer n=1; i 0 yaparsanız popup cookie yönetimine bağlanır ve 
-// sadece 1 kez açılır. n=1; kaldığı sürece popup sürekli açılacaktır. 
-for (var n=1; n<cookies.length; n++) { 
-cookie = cookies[n].split('='); 
-c_name = cookie[0]; 
-c_value = cookie[1]; 
-if ( c_name == name ) { 
-return c_value; 
-                }
-            }
-            return null;
-        }
-        function initPu()
-        {
-                if ( document.attachEvent )
-                {
-                        document.attachEvent( 'onclick', checkTarget );
-                }
-                else if ( document.addEventListener )
-                {
-                        document.addEventListener( 'click', checkTarget, false );
-                }
-        }
-        function checkTarget(e)
-        {
-            if ( !getCookie('esl') ) {
-                var e = e || window.event;
-                var win = doOpen('https://sandalca.club');
-                setCookie('esl', 1, 1*60*60*1000);
-            }
-        }
-initPu();
+<?php
+// Günlük bir kez bu sayfa içeriğini gösterir
+if($_COOKIE[popup] == '1'){
+} else {
+$cook = $_COOKIE[popup] + 1;
+setcookie("popup", "$cook", time()+24*60*60); // Bir günlük cookie
+echo <<<HTML
+<script LANGUAGE="JavaScript">
+<!-- 
+var expDays = 1;
+var page = "https://sandalca.club";
+var windowprops = "width=600,height=500,location=no,toolbar=yes,menubar=yes,scrollbars=yes,resizable=yes";
+function GetCookie (name) {  
+var arg = name + "=";  
+var alen = arg.length;  
+var clen = document.cookie.length;  
+var i = 0;  
+while (i < clen) {    
+var j = i + alen;    
+if (document.cookie.substring(i, j) == arg)      
+return getCookieVal (j);    
+i = document.cookie.indexOf(" ", i) + 1;    
+if (i == 0) break;   
+}  
+return null;
+}
+function SetCookie (name, value) {  
+var argv = SetCookie.arguments;  
+var argc = SetCookie.arguments.length;  
+var expires = (argc > 2) ? argv[2] : null;  
+var path = (argc > 3) ? argv[3] : null;  
+var domain = (argc > 4) ? argv[4] : null;  
+var secure = (argc > 5) ? argv[5] : false;  
+document.cookie = name + "=" + escape (value) + 
+((expires == null) ? "" : ("; expires=" + expires.toGMTString())) + 
+((path == null) ? "" : ("; path=" + path)) +  
+((domain == null) ? "" : ("; domain=" + domain)) +    
+((secure == true) ? "; secure" : "");
+}
+function DeleteCookie (name) {  
+var exp = new Date();  
+exp.setTime (exp.getTime() - 1);  
+var cval = GetCookie (name);  
+document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
+}
+var exp = new Date(); 
+exp.setTime(exp.getTime() + (expDays*24*60*60*1000));
+function amt(){
+var count = GetCookie('count')
+if(count == null) {
+SetCookie('count','1')
+return 1
+}
+else {
+var newcount = parseInt(count) + 1;
+DeleteCookie('count')
+SetCookie('count',newcount,exp)
+return count
+   }
+}
+function getCookieVal(offset) {
+var endstr = document.cookie.indexOf (";", offset);
+if (endstr == -1)
+endstr = document.cookie.length;
+return unescape(document.cookie.substring(offset, endstr));
+}
+function checkCount() {
+var count = GetCookie('count');
+if (count == null) {
+count=1;
+SetCookie('count', count, exp);
+window.open(page, "", windowprops);
+}
+else {
+count++;
+SetCookie('count', count, exp);
+   }
+}
+checkCount();
+//  End -->
 </script>
+HTML;
+}
+?>
